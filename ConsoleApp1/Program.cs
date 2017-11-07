3d5ace571ae8657b26e216a5ee3050a4e65ebd9c
Paul Chapman <poulchapman@users.noreply.github.com>
2017-11-07 15:11:25 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Random rndNum = new Random(int.Parse(Guid.NewGuid().ToString().Substring(0, 8), System.Globalization.NumberStyles.HexNumber));
+                int randomNumber = rndNum.Next(0, 85);
+                Console.WriteLine(randomNumber);
+            }
+        }
+    }
+}
+
